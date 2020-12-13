1efc85905dd13d69fcf7bf983585070b768f2f9e
ZodyT <Zoraida1988@yahoo.com>
2020-12-13 10:26:08 -0600

    Tests

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -14,49 +14,65 @@ namespace UnitTestingExercise.Tests
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
-            var add = new Add();
+            var calculator= new UnitTestMethods();
 
             //Act
-            var actual = num1 + num2 + num3;
+            var actual = calculator.Add(num1, num2, num3);
 
             //Assert
-            Asset.Equal(expected, actual);
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(1, 2, -1)]
+        [InlineData(24, 15, 9)]
+        [InlineData(33, 45, -12)]
+        [InlineData(50, 25, 25)]
         public void Subtract(int minuend, int subtrhend, int expected)
         {
             //Arrange
+            var calculator = new UnitTestMethods();
 
             //Act
+            var actual = calculator.Subtract(minuend, subtrhend);
 
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(1, 2, 2)]
+        [InlineData(4, 5, 20)]
+        [InlineData(7, 8, 56)]
+        [InlineData(10, 11, 110)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
+            var calculator = new UnitTestMethods();
 
             //Act
+            var actual = calculator.Multiply(num1, num2);
 
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(2, 2, 1)]
+        [InlineData(20, 5, 4)]
+        [InlineData(80, 10, 8)]
+        [InlineData(100, 2, 50)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
+            var calculator = new UnitTestMethods();
 
             //Act
+            var actual = calculator.Divide(num1, num2);
 
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Fact]
